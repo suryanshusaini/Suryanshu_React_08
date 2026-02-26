eef6232dfb4de8cc826ae1beff902c7d81bf4396
Surysnahu Saini <suryanshusaini2009@gmail.com>
2026-02-26 17:21:17 +0530

    feat: add custom React implementation

---
+++ customReact/customreact.js
@@ -0,0 +1,31 @@
+function customRender(reactElement, container) {
+  /*
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  container.appendChild(domElement);
+  */
+  /* this below code is much better than above */
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  for (const prop in reactElement.props) {
+    if (prop === "children") continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+  container.appendChild(domElement);
+}
+/* in real react also there will be a method which continuously create element and elements are like a tree graph that we can see below and w inject it like that */
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https.//google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
+
+customRender(reactElement, mainContainer);
