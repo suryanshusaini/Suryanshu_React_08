e347e16d9cc0ff2287d74b8d2e9bb1da2aef5ae8
Surysnahu Saini <suryanshusaini2009@gmail.com>
2026-02-27 20:38:41 +0530

    Add Card component to render dynamic user data via props

--- 02tailwindprops/src/App.jsx
+++ 02tailwindprops/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Card from "./components/Cards";
 function App() {
-  const [count, setCount] = useState(0);
+  // let myObj = {
+  //   username: "suryanshu saini",
+  //   age: 21,
+  // };
 
+  let newArr = [1, 2, 3];
   return (
     <>
-      <h1>Tailwind Test</h1>
+      <h1 className="bg-green-400 text-black p-4 m-50 rounded-xl">
+        Tailwind Test
+      </h1>
+      {/* <Card channel="practicereact" someObj={myObj} /> */}
+      <Card username="surya" price="0.5" />
+      <Card username="parker" />
     </>
   );
 }
