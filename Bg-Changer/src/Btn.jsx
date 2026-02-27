d64a11ece598860e26b1a8dd1fbc433c0ee0099c
Surysnahu Saini <suryanshusaini2009@gmail.com>
2026-02-27 21:43:47 +0530

    add comments

--- Bg-Changer/src/Btn.jsx
+++ Bg-Changer/src/Btn.jsx
@@ -2,7 +2,7 @@ function Btn({ color, onColorChange }) {
   return (
     <>
       <button
-        onClick={() => onColorChange(color)}
+        onClick={() => onColorChange(color)} // actually on click demand fuction not value that why we have used ()=> the change color
         style={{
           backgroundColor: color,
           padding: "10px 20px",
